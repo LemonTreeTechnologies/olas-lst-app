c047d2ef70667f600136ed0d041f30dc6fe26ff8
Atatakai <densatsu@mail.ru>
2025-05-11 20:32:50 +0400

    fix: return sepolia chain as default

--- src/hooks/useChainId.ts
+++ src/hooks/useChainId.ts
@@ -1,11 +1,12 @@
-import { mainnet } from "viem/chains";
+import { sepolia } from "viem/chains";
 import { useAccount } from "wagmi";
 import { networks } from "@/config/wagmi";
 
 export const useChainId = () => {
   const { chainId } = useAccount();
 
-  if (!chainId) return mainnet.id;
+  // TODO: temporarily return sepolia, once live replace with mainnet
+  if (!chainId) return sepolia.id;
   if (!networks.map((network) => network.id).includes(chainId)) {
     throw new Error("Network not supported");
   }
