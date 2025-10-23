ca35e9e2e18c0c6b1632f1b3352b69dee6a67cb8
Atatakai <densatsu@mail.ru>
2025-10-24 02:22:47 +0400

    feat: update footer (#22)

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,9 +1,42 @@
+import { FaGithub, FaTelegramPlane } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 const CURRENT_YEAR = new Date().getFullYear();
 
 export const Footer = () => {
   return (
-    <footer className="flex items-center justify-center font-tertiary">
-      {`© stOLAS ${CURRENT_YEAR}`}
+    <footer className="flex gap-2 items-center justify-center font-tertiary">
+      <span>{`© stOLAS ${CURRENT_YEAR} `}</span>
+      <span>•</span>
+      <a
+        href="https://x.com/lst_olas"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaXTwitter />
+      </a>
+      <a
+        href="https://t.me/+2f8-MR06jHllYjVk"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTelegramPlane />
+      </a>
+      <a
+        href="https://github.com/LemonTreeTechnologies/olas-lst"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub />
+      </a>
+      <span>•</span>
+      <a
+        href="https://github.com/CODESPECT-security/audit-reports/blob/main/034_CODESPECT_LSTOLAS.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Audit link
+      </a>
     </footer>
   );
 };
