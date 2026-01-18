4e16002bda67581b0488be2af805990dfab0a7d0
Atatakai <densatsu@mail.ru>
2026-01-18 20:19:41 +0100

    style: fix mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,14 +24,15 @@ export default function Home() {
       </Head>
 
       <LaunchLayout>
-        <div className="flex flex-col flex-auto gap-6 p-12 items-center transition-opacity duration-700 opacity-100 bg-[url('/images/waterfall.png')] bg-no-repeat bg-contain">
-          <h1 className="text-5xl font-bold font-gradient text-center leading-tight overflow-visible">
+        <div className="flex flex-col flex-auto gap-6 py-8 md:p-12 items-center transition-opacity duration-700 opacity-100 md:bg-[url('/images/waterfall.png')] bg-no-repeat bg-bottom bg-contain">
+          <div className="flex fex-col flex-auto absolute -left-12 -right-12 top-0 bottom-12 bg-[url('/images/waterfall.png')] bg-no-repeat bg-bottom bg-contain md:bg-none" />
+          <h1 className="text-4xl md:text-5xl md:leading-tight font-bold font-gradient text-center overflow-visible">
             Liquid staking with stOLAS
           </h1>
           <p className="font-tertiary text-lg text-center">
             Scalable rewards powered by a modular framework
           </p>
-          <div className="flex gap-12">
+          <div className="flex gap-8 md:gap-12">
             <div className="flex flex-col gap-1 text-center">
               <span className="font-gradient text-4xl font-semibold">
                 {isStTotalAssetsLoading ? (
